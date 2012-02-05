8ab77b088c9812bf43958abe71d21648ec6ec9c2
bverhue <bverhue@15fa39d2-16fa-406d-8567-6e833b72dc49>
2012-02-05 20:21:42 +0000

    git-svn-id: https://svn.code.sf.net/p/nmg2editor/code/trunk@5 15fa39d2-16fa-406d-8567-6e833b72dc49

--- Source/NMG2Controls/fastbitmap.pas
+++ Source/NMG2Controls/fastbitmap.pas
@@ -1,5 +1,37 @@
 unit fastbitmap;
 
+//  ////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2011 Bruno Verhue
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  ////////////////////////////////////////////////////////////////////////////
+
+// Sources:
+
+// Polygon filling
+// http://www.sunshine2k.de/stuff/Java/Polygon/Filling/FillPolygon.htm
+// http://www.cs.rit.edu/~icss571/filling/index.html
+
+// Anti aliased disc:
+// Nils Haeck M.Sc. www.simdesign.nl
+
+// FPC Scanline alternatives
+// http://wiki.lazarus.freepascal.org/Developing_with_Graphics
+
 {$IFDEF FPC}
 {$mode objfpc}{$H+}
 {$ENDIF}
